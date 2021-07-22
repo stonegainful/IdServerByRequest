a492ec1f2cae36bd868abd1238168a83c1f8d8aa
Stonegainful <stonegainful@gmail.com>
2021-07-21 21:02:49 -0600

    Add batch URL process from file

--- IdServerByRequest.py
+++ IdServerByRequest.py
@@ -31,35 +31,42 @@ def main():
     now = datetime.now() # current date and time
 
     if (option == 2):
-        urlFile = input('Ingrese IP Address(s) / URL(s):')
+        urlFile = input('Ingrese el nombre y ubicacion del archivo de entrada:')
+        f = open(urlFile, "r")
+        urls = []
+        for line in f:
+            urls.append(line[0:len(line)-1])
+        f.close()
 
-        try:
-            #for url in textfile:
-            pathOutput = input("Ingrese el nombre del archivo de salida: ")
-            filename, ext = os.path.splitext(pathOutput)
-            if not ext:
-                ext = '.csv'
-            pathOutput = filename + ext
+        #for url in textfile:
+        pathOutput = input("Ingrese el nombre del archivo de salida: ")
+        filename, ext = os.path.splitext(pathOutput)
+        if not ext:
+            ext = '.csv'
+        pathOutput = filename + ext
 
-            response = requests.request("GET", urlFile, verify=True)
-            headServer = response.headers.get('Server')
-            headPoweredBy = response.headers.get('X-Powered-By')
-            headAspVer = response.headers.get('X-AspNet-Version')
+        headerCols = ("Nombre del Sitio","Server","X-Powered-By","X-AspNet-Version","Evidencia")
+        with open(pathOutput, 'w', newline='') as csvfile:
+            textWriter = csv.writer(csvfile, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            textWriter.writerow(headerCols)
+            for url in urls:
+                try:
+                    response = requests.request("GET", url, verify=True)
+                    headServer = response.headers.get('Server')
+                    headPoweredBy = response.headers.get('X-Powered-By')
+                    headAspVer = response.headers.get('X-AspNet-Version')
 
-            if headServer:
-                print('Server:', headServer, '\n')
-            if headPoweredBy:
-                print('X-Powered-By:', headPoweredBy, '\n')
-            if headAspVer:
-                print('X-AspNet-Version:', headAspVer, '\n')
-                
-            headerCols = ("Nombre del Sitio","Server","X-Powered-By","X-AspNet-Version","Evidencia")
-            with open(pathOutput, 'w', newline='') as csvfile:
-                spamwriter = csv.writer(csvfile, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-                spamwriter.writerow(headerCols)
-                spamwriter.writerow([urlFile, headServer, headPoweredBy, headAspVer])
-        except ConnectionError:
-            print ('Se omite la URL {url} no se obtuvo respuesta')
+                    if headServer:
+                        print('Server:', headServer)
+                    if headPoweredBy:
+                        print('X-Powered-By:', headPoweredBy)
+                    if headAspVer:
+                        print('X-AspNet-Version:', headAspVer)
+                        
+                    textWriter.writerow([url, headServer, headPoweredBy, headAspVer])
+
+                except ConnectionError:
+                    print (f'Se omite la URL {url} no se obtuvo respuesta')
 
         print('\n[**] Proceso finalizado \n')
         print(f'[**] Resultado almacenado en {pathOutput} \n')
@@ -90,15 +97,18 @@ def main():
                 
             headerCols = ("Nombre del Sitio","Server","X-Powered-By","X-AspNet-Version","Evidencia")
             with open(pathOutput, 'w', newline='') as csvfile:
-                spamwriter = csv.writer(csvfile, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-                spamwriter.writerow(headerCols)
-                spamwriter.writerow([url, headServer, headPoweredBy, headAspVer])
+                textWriter = csv.writer(csvfile, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+                textWriter.writerow(headerCols)
+                textWriter.writerow([url, headServer, headPoweredBy, headAspVer])
         except ConnectionError:
             print ('Ha fallado obtener respuesta de la URL {url}')
 
         print('\n[**] Proceso finalizado. \n')
         print(f'[**] Resultado almacenado en {pathOutput} \n')
         main()
+
+    if (option == 3):
+        print("\n[**] Finalizando el script... Gracias por usar la herramienta \n")
 
     else:
         print("\nOpcion no valida... Intente de nuevo otra vez >>\n")
